1dba50acef8af2267292102d45bad2a446611bdc
Daniel <danielminami@hotmail.com>
2020-06-11 08:56:19 -0400

    Implementation of Calculator Class.

---
+++ Assignment2/models/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2.models {
+    class Calculator {
+
+        public double Num1 { get; set; }
+        public double Num2 { get; set; }
+        public string Oper { get; set; }
+
+        public string calculate (double n1, double n2, string op) {
+            double result = 0;
+            switch (op) {
+                case "+":
+                    result = n1 + n2;
+                    break;
+                case "-":
+                    result = n1 - n2;
+                    break;
+                case "*":
+                    result = n1 * n2;
+                    break;
+                case "/":
+                    if (n1 == 0 || n2 == 0)
+                        return "Error: Division by zero";
+                    result = n1 / n2;
+                    break;
+                case "%":
+                    result = n1 % n2;
+                    break;
+            }
+            return result.ToString();
+        }
+
+    }
+}
