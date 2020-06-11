7f447b1120610ebb87cda9c76febb5aa2eec1c4b
Daniel <danielminami@hotmail.com>
2020-06-11 14:44:21 -0400

    Added Mem class to store memorized values.

---
+++ Assignment2/models/Mem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2.models {
+    class Mem {
+        public double Num1 { get; set; }
+
+        public void clear() {
+            Num1 = 0;
+        }
+    }
+}
